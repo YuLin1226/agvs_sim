d0603e4433235f8c2b6d51212d4d24f1c3f7ecff
RyanChen_YLC <r08522622@ntu.edu.tw>
2022-03-15 16:10:42 +0800

    add pkg_install shell

---
+++ agvs_navigation/pkg_install.sh
@@ -0,0 +1,24 @@
+sudo apt -y install ros-noetic-joy
+
+sudo apt -y install ros-noetic-move-base
+
+sudo apt -y install ros-noetic-map-server
+
+sudo apt -y install ros-noetic-amcl
+
+sudo apt -y install ros-noetic-robot-localization
+
+sudo apt -y install ros-noetic-dwa-local-planner
+
+sudo apt -y install ros-noetic-teb-local-planner
+
+sudo apt -y install ros-noetic-rqt
+
+sudo apt -y install ros-noetic-global-planner
+
+sudo apt -y install ros-noetic-effort-controllers
+
+sudo apt -y install ros-noetic-rosserial
+
+sudo apt -y install ros-noetic-ros-control
+
